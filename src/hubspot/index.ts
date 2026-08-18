8a8c035386f07140a0af36eeefc9cf15a4461fa4
Kevin Bibelhausen <kevin.bibelhausen@gmail.com>
2026-08-18 01:04:08 -0400

    feat(hubspot): export outgoing-email logging constants from index
    EMAIL_TO_CONTACT_TYPE_ID, EMAIL_TO_COMPANY_TYPE_ID, DEFAULT_INTERNAL_EMAIL_DOMAINS,
    and the LogOutgoingEmailInput type join the hubspot/index.ts barrel.
    
    Part of chip-hubspot-email-log PR-A.

--- src/hubspot/index.ts
+++ src/hubspot/index.ts
@@ -1,2 +1,2 @@
-export { HubSpotClient } from './client.js';
-export type { HubSpotClientConfig, HubSpotFilter, HubSpotFilterGroup, HubSpotSearchSort, HubSpotSearchOpts } from './client.js';
+export { HubSpotClient, EMAIL_TO_CONTACT_TYPE_ID, EMAIL_TO_COMPANY_TYPE_ID, DEFAULT_INTERNAL_EMAIL_DOMAINS } from './client.js';
+export type { HubSpotClientConfig, HubSpotFilter, HubSpotFilterGroup, HubSpotSearchSort, HubSpotSearchOpts, LogOutgoingEmailInput } from './client.js';
